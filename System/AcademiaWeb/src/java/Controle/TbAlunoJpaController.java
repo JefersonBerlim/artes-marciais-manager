4177023ec10822df0ece01ad49c1996d74946c25
Jeferson Berlim <jeferson.berlim@gmail.com>
2015-06-04 15:46:31 -0300

    Alteração do Projeto para trabalhar com firebird alias
    Alteração do Projeto para trabalhar com firebird alias

--- System/AcademiaWeb/src/java/Controle/TbAlunoJpaController.java
+++ System/AcademiaWeb/src/java/Controle/TbAlunoJpaController.java
@@ -13,8 +13,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 /**
  *
